20cc5b6e3beb87f807a78619774aaaeeca806d89
Kizuru <fr.kizuru@gmail.com>
2020-01-02 23:05:43 +0100

    Some update

--- client/src/main.js
+++ client/src/main.js
@@ -3,10 +3,11 @@ import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
+// noinspection JSUnusedGlobalSymbols
 new Vue({
   router,
   vuetify,
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
